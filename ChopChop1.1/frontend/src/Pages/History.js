d0531157f2bcef50c15002ccc22680038bd6e571
Soon Xiang, Yan <65216986+y4ns00n@users.noreply.github.com>
2020-06-05 05:50:47 +0800

    added functionality to hist and fav pages

--- ChopChop1.1/frontend/src/Pages/History.js
+++ ChopChop1.1/frontend/src/Pages/History.js
@@ -11,6 +11,63 @@ class History extends Component {
     };
   }
 
+  handleDelete = (e) => {
+    const list = document.querySelector("#book-list ul");
+
+    if (e.target.className == "delete") {
+      const li = e.target.parentElement;
+      list.removeChild(li);
+    }
+  };
+
+  handleAdd = (e) => {
+    e.preventDefault();
+
+    const list = document.querySelector("#book-list ul");
+    const forms = document.forms;
+
+    const addForm = forms["add-book"];
+
+    //creating elements
+    const value = addForm.querySelector('input[type = "text"]').value;
+    const li = document.createElement("li");
+    const histItem = document.createElement("span");
+    const deleteBtn = document.createElement("span");
+
+    //adding text content
+    histItem.textContent = value;
+    deleteBtn.textContent = "delete";
+
+    //tagging classes to the items
+    histItem.classList.add("name");
+    deleteBtn.classList.add("delete");
+    deleteBtn.addEventListener("click", this.handleDelete);
+
+    //appending to the domm
+    li.appendChild(histItem);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+  };
+
+  handleSearch = (e) => {
+    const list = document.querySelector("#book-list ul");
+    const forms = document.forms;
+    const searchBar = forms["search-books"].querySelector("input");
+
+    searchBar.addEventListener("keyup", (e) => {
+      const term = e.target.value.toLowerCase();
+      const books = list.getElementsByTagName("li");
+      Array.from(books).forEach((book) => {
+        const title = book.firstElementChild.textContent;
+        if (title.toLowerCase().indexOf(e.target.value) != -1) {
+          book.style.display = "block";
+        } else {
+          book.style.display = "none";
+        }
+      });
+    });
+  };
+
   render() {
     return (
       <div style={{ paddingTop: "40px" }}>
@@ -20,7 +77,11 @@ class History extends Component {
             <div id="page-banner">
               <h1 class="title">History Page</h1>
               <form id="search-books">
-                <input type="text" placeholder="Search History..." />
+                <input
+                  type="text"
+                  placeholder="Search History..."
+                  onChange={this.handleSearch}
+                />
               </form>
             </div>
           </header>
@@ -29,27 +90,33 @@ class History extends Component {
             <ul>
               <li>
                 <span class="name">Mcdonalds</span>
-                <span class="delete">delete</span>
+                <span class="delete" onClick={this.handleDelete}>
+                  delete
+                </span>
               </li>
               <li>
                 <span class="name">KFC</span>
-                <span class="delete">delete</span>
+                <span class="delete" onClick={this.handleDelete}>
+                  delete
+                </span>
               </li>
               <li>
                 <span class="name">East Coast Park</span>
-                <span class="delete">delete</span>
+                <span class="delete" onClick={this.handleDelete}>
+                  delete
+                </span>
               </li>
               <li>
                 <span class="name">Changi Beach</span>
-                <span class="delete">delete</span>
+                <span class="delete" onClick={this.handleDelete}>
+                  delete
+                </span>
               </li>
             </ul>
           </div>
           <form id="add-book">
-            <input type="checkbox" id="hide" />
-            <label for="hide">Hide History</label>
             <input type="text" placeholder="Add a Place..." />
-            <button>Add</button>
+            <button onClick={this.handleAdd}>Add</button>
           </form>
         </div>
       </div>
